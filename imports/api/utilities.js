6fac289691cef287da2189146a6cc4a6ac72fd50
Josh <josh@joshjoe.com>
2018-07-24 21:34:37 -0500

    Now passing current month to calculate function for better testing

--- imports/api/utilities.js
+++ imports/api/utilities.js
@@ -1,3 +1,5 @@
+import moment from 'moment';
+
 /* eslint import/prefer-default-export: "off" */
 export const calculateDaysInPlacement = (
   endDate,
@@ -5,21 +7,41 @@ export const calculateDaysInPlacement = (
   isActive,
   year,
   month,
-  currentYearMonth
+  currentYearMonth,
+  todaysDate
 ) => {
   const startOfMonth = `${String(year)}-${String(month)}`;
+  const endOfMonth = `${String(year)}-${String(month)}-${new Date(
+    year,
+    month,
+    0
+  ).getDate()}`;
+
+  const startDateObject = new Date(startDate);
+  const endDateObject = endDate === null ? null : new Date(endDate);
 
   if (currentYearMonth === startOfMonth) {
     if (isActive) {
-      if (new Date(startOfMonth) < startDate) {
-        return new Date().getDate() - startDate.getDate();
+      if (new Date(startOfMonth) < startDateObject) {
+        return todaysDate - startDateObject.getDate();
       }
       // This should be the number of days from the first day of the month to today (including today)
-      return new Date().getDate() + 1;
+      return todaysDate + 1;
     }
-    if (new Date(startOfMonth) < startDate) {
-      return endDate.getDate() - startDate.getDate() + 1;
+    if (new Date(startOfMonth) < startDateObject) {
+      return endDateObject.getDate() - startDateObject.getDate() + 1;
     }
-    return endDate.getDate() - 1;
+    if (endDateObject < new Date(startOfMonth)) {
+      return 0;
+    }
+    // + 2 because the end date will be set to the 1st second of the day, and that means that it won't be counted
+    return endDateObject.getDate() + 2;
   }
+  if (new Date(startOfMonth) < startDateObject) {
+    if (new Date(endOfMonth) < startDateObject) {
+      return 0;
+    }
+    return new Date(endOfMonth).getDate() - startDateObject.getDate();
+  }
+  return new Date(endOfMonth).getDate();
 };

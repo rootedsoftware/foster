6c0715c20a6a5011dfab1d5adc9f09109a527ced
Frank <frank@Franks-MBP.lan>
2018-08-21 22:36:02 -0500

    active route helper

--- imports/ui/components/header/header.js
+++ imports/ui/components/header/header.js
@@ -1,7 +1,11 @@
+import { Template } from 'meteor/templating';
 import './header.html';
 
 Template.Header.helpers({
-  'isActiveRoute'(name) {
-    return FlowRouter.getRouteName() && FlowRouter.getRouteName().toLowerCase() === name ? 'active' : '';
-  }
-})
+  isActiveRoute(name) {
+    return FlowRouter.getRouteName()
+      && FlowRouter.getRouteName().toLowerCase() === name
+      ? 'active'
+      : '';
+  },
+});

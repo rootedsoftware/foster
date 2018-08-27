c2a3bdd546173ba9de265bf3aa4fa575603b41a2
Joshua Bechard <josh.bechard@gmail.com>
2018-08-27 00:50:42 -0500

    Removed unused var

--- imports/ui/components/placements/Placement.js
+++ imports/ui/components/placements/Placement.js
@@ -1,4 +1,3 @@
-import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 import Placements from '../../../api/placements/placements';
 import { placementRemove } from '../../../api/placements/methods';

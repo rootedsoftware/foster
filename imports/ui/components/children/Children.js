dfd1697f4d9786f0768ce64136ba3b4d622e69fe
Joshua Bechard <josh.bechard@gmail.com>
2018-08-29 08:51:00 -0500

    Children split into two components

--- imports/ui/components/children/Children.js
+++ imports/ui/components/children/Children.js
@@ -2,6 +2,7 @@ import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 import { childInsert, childRemove } from '../../../api/children/methods';
 import Children from '../../../api/children/children';
+import './AddChild';
 import './Children.html';
 import { showToast } from '../../../api/utilities';
 
